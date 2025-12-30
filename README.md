e1cc87ebbe77937b502e5a710ad095c39f251162
Benjamin King <benjamin.allan.king@gmail.com>
2025-12-30 16:06:11 +0000

    Update for Fonoa

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+Build: `npm i && npm run build`
+Dev: `npm i && npm run dev`
--- public/data.js
+++ public/data.js
@@ -1,8 +1,30 @@
 export default {
   jobs: {
-    Ripjar: "2016 - present",
+    Ripjar: "2016 - present"
   },
   experience: [
+    {
+      words: [
+        { text: "golang", weight: 10 },
+        { text: "automation", weight: 6 },
+        { text: "postgres", weight: 6 },
+        { text: "typescript", weight: 8 },
+        { text: "redis", weight: 4 },
+        {
+          text: "gcp",
+          weight: 8
+        }
+      ],
+      company: "Fonoa",
+      date: "10/2024 - present",
+      role: "Senior Engineer",
+      description: [
+        "Became an IC again at financial scale-up",
+        "Increasing automation techniques for web scraping",
+        "Tranforming enterprise system to multi-tenant SaaS platform",
+        "Improving performance with parallel processing"
+      ]
+    },
     {
       words: [
         { text: "typescript", weight: 10 },
@@ -10,82 +32,81 @@ export default {
         { text: "design", weight: 6 },
         {
           text: "kafka",
-          weight: 8,
+          weight: 8
         },
         { text: "events", weight: 4 },
         { text: "microservices", weight: 4 },
         {
           text: "distributed systems",
-          weight: 4,
+          weight: 4
         },
         {
           text: "ci/cd",
-          weight: 4,
+          weight: 4
         },
         {
           text: "streaming",
-          weight: 4,
+          weight: 4
         },
         {
           text: "github actions",
-          weight: 4,
-        },
+          weight: 4
+        }
       ],
       company: "Ripjar",
-      date: "01/2022 - present",
+      date: "01/2022 - 09/2024",
       role: "Staff Engineer",
       description: [
         "Holistic technical responsibility over engineering teams and processes",
         "Coordinating business, product and engineering roadmaps with senior stakeholders",
-        "Designing and building next iteration of multi-tenant financial crime SaaS product",
+        "Designing and building next iteration of financial crime detection product",
         "Fleshing out system level tests and fitness functions",
-        "Capturing performance metrics and monitoring",
         "Improving developer experience and efficiency",
-        "Revamped test automation and CI pipeline from Jenkins to docker and Github Actions",
-      ],
+        "Revamped test automation and CI pipeline from Jenkins to docker and Github Actions"
+      ]
     },
     {
       words: [
         {
           text: "leadership",
-          weight: 12,
+          weight: 12
         },
         {
           text: "mentoring",
-          weight: 12,
+          weight: 12
         },
         {
           text: "ansible",
-          weight: 4,
+          weight: 4
         },
         {
           text: "jenkins",
-          weight: 10,
+          weight: 10
         },
         {
           text: "scrum",
-          weight: 6,
+          weight: 6
         },
         {
           text: "nodejs",
-          weight: 8,
+          weight: 8
         },
         {
           text: "opensearch",
-          weight: 6,
+          weight: 6
         },
         {
           text: "mongodb",
-          weight: 6,
+          weight: 6
         },
         {
           text: "redis",
-          weight: 6,
+          weight: 6
         },
         {
           text: "jira",
-          weight: 6,
-        },
+          weight: 6
+        }
       ],
       company: "Ripjar",
       date: "09/2019 - 01/2022",
@@ -95,47 +116,47 @@ export default {
         "Planning sprints; requirements gathering and task breakdown",
         "Line management of 5 team members including their mentoring and career progression",
         "Recruitment lead for full-stack and front-end developers",
-        "Responsibility for application with 40,000 users in the world’s biggest financial institutions",
-      ],
+        "Responsibility for application with 40,000 users in the world’s biggest financial institutions"
+      ]
     },
     {
       words: [
         {
           text: "javascript",
-          weight: 15,
+          weight: 15
         },
         {
           text: "react",
-          weight: 10,
+          weight: 10
         },
         {
           text: "team lead",
-          weight: 12,
+          weight: 12
         },
         {
           text: "webpack",
-          weight: 4,
+          weight: 4
         },
         {
           text: "git",
-          weight: 10,
+          weight: 10
         },
         {
           text: "d3",
-          weight: 10,
+          weight: 10
         },
         {
           text: "webgl",
-          weight: 10,
+          weight: 10
         },
         {
           text: "jest",
-          weight: 6,
+          weight: 6
         },
         {
           text: "selenium",
-          weight: 6,
-        },
+          weight: 6
+        }
       ],
       company: "Ripjar",
       date: "04/2016 - 09/2019",
@@ -146,39 +167,39 @@ export default {
         "Liaising with UX designers",
         "Involvement with initial release of network intelligence application",
         "Development lead of financial crime product's user interface",
-        "Upskilling others in React, version control and modern javascript",
-      ],
+        "Upskilling others in React, version control and modern javascript"
+      ]
     },
     {
       words: [
         {
           text: "asp.net",
-          weight: 10,
+          weight: 10
         },
         {
           text: "javascript",
-          weight: 8,
+          weight: 8
         },
         {
           text: "python",
-          weight: 6,
+          weight: 6
         },
         {
           text: "c#",
-          weight: 12,
+          weight: 12
         },
         {
           text: "geosystems",
-          weight: 4,
+          weight: 4
         },
         {
           text: "java",
-          weight: 10,
+          weight: 10
         },
         {
           text: "git",
-          weight: 6,
-        },
+          weight: 6
+        }
       ],
       company: "Helyx SIS",
       date: "07/2015 - 04/2016",
@@ -186,81 +207,80 @@ export default {
       description: [
         "Software developer integrating geo capability into existing systems",
         "Ported old VB6 system to use modern technologies",
-        "Introduced version control to company",
-      ],
+        "Introduced version control to company"
+      ]
     },
     {
       words: [
         {
           text: "java",
-          weight: 14,
+          weight: 14
         },
         {
           text: "javascript",
-          weight: 6,
+          weight: 6
         },
         {
           text: "network graph",
-          weight: 6,
+          weight: 6
         },
         {
           text: "angular js",
-          weight: 6,
+          weight: 6
         },
         {
           text: "desktop app",
-          weight: 10,
+          weight: 10
         },
         {
           text: "c++",
-          weight: 8,
+          weight: 8
         },
         {
           text: "neo4j",
-          weight: 4,
-        },
+          weight: 4
+        }
       ],
       company: "BAE Systems Detica",
       date: "06/2012 - 07/2015",
       role: "Graduate Software Developer",
       description: [
-        "Provided support of long running desktop application",
         "Built upon existing Java framework for analysis of network graphs",
         "Followed best practices for version control and continuous deployment",
         "Research project analysing website content using Natural Language Processing",
-        "Curated queryable data platform for rich scenario based content",
-      ],
+        "Curated queryable data platform for rich scenario based content"
+      ]
     },
     {
       words: [
         {
           text: "golang",
-          weight: 14,
+          weight: 14
         },
         {
           text: "nextjs",
-          weight: 6,
+          weight: 6
         },
         {
           text: "tailwind",
-          weight: 6,
+          weight: 6
         },
         {
           text: "vuejs",
-          weight: 6,
+          weight: 6
         },
         {
           text: "aws",
-          weight: 6,
+          weight: 6
         },
         {
           text: "kubernetes",
-          weight: 6,
+          weight: 6
         },
         {
           text: "firebase",
-          weight: 10,
-        },
+          weight: 10
+        }
       ],
       company: "Personal Projects",
       date: "projects",
@@ -270,26 +290,26 @@ export default {
         "godash - my first published go package https://github.com/baking-code/godash",
         "AWS - working towards developer certification",
         "portfolio - this website/PDF, using next.js, tailwind css, react pdf",
-        "recipeasy - web app to share recipes using firebase as a data store",
-      ],
-    },
+        "recipeasy - web app to share recipes using firebase as a data store"
+      ]
+    }
   ],
   home: [
     {
-      description: ["Benjamin King, staff software engineer"],
+      description: ["Benjamin King, staff software engineer"]
     },
     {
       description: [
-        "Hello there! My name is Ben and I am a full stack staff software engineer.",
-        "The majority of my recent experiences lie in building complex data-driven applications. The challenge involved in the consumption and presentation of complex data is something that I enjoy.",
-        "My current company Ripjar are a Series-B data intelligence scale up. I joined 2 years into their journey, and it has since grown over 6 times the size. I have led development of both their financial crime solution and data analysis platform.",
-        "I love playing with technology. Making myself, and others around me, better at what they do is what inspires me. 🎉",
-      ],
-    },
+        "I'm Ben, an experienced software engineer who thrives on building impactful products.",
+        "I specialize in transforming complex data challenges into elegant, intuitive business solutions that drive real value.",
+        "At Fonoa, a financial scale-up, I joined 4 years in and have been instrumental in doubling ARR year over year.",
+        "What drives me? Leveraging technology to elevate both my work and the capabilities of those around me."
+      ]
+    }
   ],
   about: [
     {
-      description: ["Here's a little bit about me outside of tech!"],
+      description: ["Here's a little bit about me outside of tech!"]
     },
     {
       description: [
@@ -297,15 +317,15 @@ export default {
         "We love exploring the countryside and tasting different cultures ✈️",
         "I try to maintain fitness with cycling, running, bouldering and yoga 🧘",
         "Avid follower of football and motorsport, and am a big fan of heavy metal music 🤘",
-        "I compete in Sim Racing events and organised leagues against of racers from around the world 🏁",
-      ],
-    },
+        "I compete in Sim Racing events and organised leagues against of racers from around the world 🏁"
+      ]
+    }
   ],
   education: [
     {
       description: [
-        "Master of mathematics, University of Exeter (2:1) 2009-2013",
-      ],
-    },
-  ],
+        "Master of mathematics, University of Exeter (2:1) 2009-2013"
+      ]
+    }
+  ]
 };
